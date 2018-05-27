a6c079c54ccfe353778d5fb647183acfccce741c
Unknown <matheusguiferreira@hotmail.com>
2018-05-27 06:23:34 -0300

    Adicionado activity da tela sobre

---
+++ app/src/main/java/mlhvproductions/controluz/AboutActivity.java
@@ -0,0 +1,66 @@
+package mlhvproductions.controluz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+/**
+ * @author Matheus G. Ferreira
+ */
+
+public class AboutActivity  extends AppCompatActivity {
+    private String TAG = "AboutActivity";
+
+    /**
+     * Cria a tela e pega um objeto do tipo Bundle como parâmetro
+     * @param savedInstanceState    Instancia salva
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        this.setTitle(getApplicationContext().getString(R.string.txtSobre));
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+}
