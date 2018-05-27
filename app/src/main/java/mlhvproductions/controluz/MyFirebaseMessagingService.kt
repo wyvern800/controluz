0adf690ef10349a3e93ecbaeca3ee3c60811bc81
Unknown <matheusguiferreira@hotmail.com>
2018-05-27 09:20:03 -0300

    Modificações

--- app/src/main/java/mlhvproductions/controluz/MyFirebaseMessagingService.kt
+++ app/src/main/java/mlhvproductions/controluz/MyFirebaseMessagingService.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.provider.Settings
 import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -29,6 +30,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val notificationBuilder = NotificationCompat.Builder(this)
         notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
         notificationBuilder.setSound(uri)
+        notificationBuilder.setVibrate(longArrayOf(500, 500))
         notificationBuilder.setStyle(inboxStyle)
         notificationBuilder.setContentText(remoteMessage.notification!!.body)
         notificationBuilder.setContentTitle(remoteMessage.notification!!.title)
